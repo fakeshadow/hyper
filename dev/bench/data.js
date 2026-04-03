8697ecead89f6a0ebc6954b18c489736807f8838
github-action-benchmark <github@users.noreply.github.com>
2026-04-03 03:37:57 +0000

    add pipeline (cargo) benchmark result for 0d6c7d5469baa09e2fb127ee3758a79b3271a4f0

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1775187477214,
+  "repoUrl": "https://github.com/fakeshadow/hyper",
+  "entries": {
+    "pipeline": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "distinct": true,
+          "id": "0d6c7d5469baa09e2fb127ee3758a79b3271a4f0",
+          "message": "v1.9.0",
+          "timestamp": "2026-03-31T11:36:23-04:00",
+          "tree_id": "7eedbbbf9baf259d1e98a4f18d2abd815e28c9f3",
+          "url": "https://github.com/fakeshadow/hyper/commit/0d6c7d5469baa09e2fb127ee3758a79b3271a4f0"
+        },
+        "date": 1775187471535,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 53078,
+            "range": "± 9951.90",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
